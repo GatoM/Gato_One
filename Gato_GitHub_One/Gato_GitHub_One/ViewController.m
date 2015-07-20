824dd23711b3346d8ae508d353412a56ad1c03f3
Gato_M <lanou3g@lanou3gdeMac-mini-16.local>
2015-07-20 15:27:26 +0800

    更改了ViewController 背景颜色

--- Gato_GitHub_One/Gato_GitHub_One/ViewController.m
+++ Gato_GitHub_One/Gato_GitHub_One/ViewController.m
@@ -17,8 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self addAllView];
 }
 
+-(void)addAllView
+{
+    self.view.backgroundColor = [UIColor cyanColor];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
