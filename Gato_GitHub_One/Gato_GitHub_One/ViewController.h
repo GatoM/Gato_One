b3fc83d10f00dc27acca9df84c358f4a31581a52
lanou3g <lanou3g@lanou3gdeMac-mini-16.local>
2015-07-20 14:54:55 +0800

    第一次提交

---
+++ Gato_GitHub_One/Gato_GitHub_One/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gato_GitHub_One
+//
+//  Created by lanou3g on 15/7/20.
+//  Copyright (c) 2015年 Gato_M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Gato_GitHub_One/Gato_GitHub_One/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Gato_GitHub_One
+//
+//  Created by lanou3g on 15/7/20.
+//  Copyright (c) 2015年 Gato_M. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
